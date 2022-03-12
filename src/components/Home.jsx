ebbfbc3e0b2a1c13fefddda4713bcebfe5d160ec
Hrishikesh Lamdade <hrishilamdade@gmail.com>
2022-03-12 10:17:47 +0530

    Loading indicator

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,6 +9,7 @@ function Home() {
     const dispatch = useDispatch();
     
     const [loading,setLoading] = useState(true)
+    const [userLoading,setUserLoading] = useState(false)
 
     useEffect(() => {
         dispatch(setAllusers()).then(()=>{
@@ -19,13 +20,14 @@ function Home() {
 
     const users = useSelector((state)=>state?.allUsers?.allUsers)
     console.log(users)
-
+    let userData = null
     const handlegetUserData = (user_id) => {
-        dispatch(setUser(user_id)).then(
-            console.log()
-        )
+        setUserLoading(true)
+        dispatch(setUser(user_id)).then(()=>{
+            setUserLoading(false)
+        })
     }
-    const userData = useSelector((state)=>state?.allUsers?.user)
+    userData = useSelector((state)=>state?.allUsers?.user)
     console.log(userData)
     
     return (
@@ -35,15 +37,26 @@ function Home() {
                 <div className='loading'><CircularProgress/><span style={{paddingLeft:'15px'}}>{" Fecthing Users..."}</span></div>
                 :
                 <div className='container'>
-                    { userData===null?<div className='user_info_note'>Please click on any user button to view its data</div>:
-                        <div className='user_info_card'>
-                            <div className='user_info_avatar_container'>
-                                <img src={userData.avatar} alt="profile" className="user_avatar" />
-                            </div>
-                            <div className='user_info_container'>
-                                <div className='user_info_name'>{userData?.first_name} {userData?.last_name}</div>
-                                <div className='user_info_label'><span style={{fontSize:'18px'}}>Email:</span> <a href={`mailto:${userData.email}`}>{userData.email}</a> </div>
-                                <div className='user_info_id'> Id : {userData.id} </div>
+                    { userData===null? <div className='user_info_note'>
+                            Please click on any user button to view its data
+                        </div> : 
+                        userLoading===true?<div className='user_info_note'>
+                            <CircularProgress style={{color:'white'}}/>
+                            <span style={{paddingLeft:'15px'}}>
+                                {" Fecthing User Data..."}
+                            </span>
+                        </div>
+                        :
+                        <div className='user_info'>
+                            <div className='user_info_card'>
+                                <div className='user_info_avatar_container'>
+                                    <img src={userData.avatar} alt="profile" className="user_avatar" />
+                                </div>
+                                <div className='user_info_container'>
+                                    <div className='user_info_name'>{userData?.first_name} {userData?.last_name}</div>
+                                    <div className='user_info_label'><span style={{fontSize:'18px'}}>Email:</span> <a href={`mailto:${userData.email}`}>{userData.email}</a> </div>
+                                    <div className='user_info_id'> Id : {userData.id} </div>
+                                </div>
                             </div>
                         </div>
                     }
